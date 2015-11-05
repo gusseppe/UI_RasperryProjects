5e60b455b6eddabf0cac94d443f6d76f1228f084
leticia <leticiapintoalva@gmail.com>
2015-11-05 16:45:12 -0500

    cambio lety

--- js/tiles.js
+++ js/tiles.js
@@ -22,9 +22,9 @@ tiles = function(){ /*Insert your own tiles here*/
 	/*GROUP 2 */
 	
 	
-	tileImg(1,0,0,1,1,"#F60","external:gotolink:links/fotos.php","img/bg/img_square.jpg",1,1,['Fotos evento','#789600','bottom','DirecTV',true],'');
-	tileImg(1,1,0,1,1,"#F90","external:gotolink:links/fotos.php","img/bg/img_square.jpg",1,1,['Fotos','#F90','top','DirectTV',true],'');
-	tileImg(1,2,0,1,1,"#C33","external:gotolink:links/subir.php","img/bg/img_square.jpg",1,1,['Subir Fotos','#C33','bottom','Click Aqui',false],'');
+	tileImg(1,0,0,1,1,"#F60","external:gotolink:links/fotos.php","img/bg/beneficios2.png",1,1,['Fotos evento','#789600','bottom','DirecTV',true],'');
+	tileImg(1,1,0,1,1,"#F90","external:gotolink:links/fotos.php","img/bg/valores.png",1,1,['Fotos','#F90','top','DirectTV',true],'');
+	tileImg(1,2,0,1,1,"#C33","external:gotolink:links/subir.php","img/bg/perfiles.png",1,1,['Subir Fotos','#C33','bottom','Click Aqui',false],'');
 	
 	/*GROUP 3 */
 	/*Fadeslide and fliphorizontal are effects, other options are 'fade','flipvertical'*/
