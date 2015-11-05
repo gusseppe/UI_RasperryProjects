182a7972478b22e0409281ecca6a01bee5d1e144
leticia <leticiapintoalva@gmail.com>
2015-11-05 18:47:38 -0500

    fotodirectv

--- links/fotos3.php
+++ links/fotos3.php
@@ -5,9 +5,12 @@
 <meta charset=utf-8 />
 <meta name="description" content="description">
  
-<title>CSS</title>
  
 <link rel="stylesheet" type="text/css" media="screen" href="css/css.css"/>
+
+  <title>Fotos</title>
+
+
  
 <!--[if IE]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
